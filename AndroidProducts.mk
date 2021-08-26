b408e5edc3279c140da424aacc775cd9b7243b53
daviiid99 <davidmolinamorales1999@gmail.com>
2021-08-26 13:32:34 +0200

    Samsung j53gxx source

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2017-2021 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+LOCAL_PATH := device/samsung/j53gxx
+PRODUCT_MAKEFILES := \
+	$(LOCAL_DIR)/lineage_j53gxx.mk
+
+COMMON_LUNCH_CHOICES := \
+    lineage_j53gxx-eng \
+    lineage_j53gxx-userdebug \
+	lineage_j53gxx-user
---
+++ device.mk
@@ -0,0 +1,19 @@
+#
+# Copyright (C) 2017-2021 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+#Inherit from vendor
+$(call inherit-product, vendor/samsung/j53gxx/j53gxx-vendor.mk)
+
+# Inherit from common
+$(call inherit-product, device/samsung/j5-common/device-common.mk)
+
+LOCAL_PATH := device/samsung/j53gxx
+
+# System properties
+-include $(LOCAL_PATH)/system_prop.mk
+
+# Common overlay
+DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
