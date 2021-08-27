7b0c89ad4fbf60b983b7ae68da52d5bd547fd22a
David <davidmolinamorales1999@gmail.com>
2021-08-27 18:38:54 +0200

    Remove PixelLauncher

--- device.mk
+++ device.mk
@@ -18,8 +18,7 @@ LOCAL_PATH := device/samsung/j53gxx
 #GAPPS config
 GAPPS_VARIANT := pico
 GAPPS_PRODUCT_PACKAGES += Youtube
-GAPPS_PRODUCT_PACKAGES += GoogleDialer
-GAPPS_PRODUCT_PACKAGES += PixelLauncher 
+GAPPS_PRODUCT_PACKAGES += GoogleDialer 
 GAPPS_PRODUCT_PACKAGES += Wallpapers
 $(call inherit-product, vendor/opengapps/build/opengapps-packages.mk)
 
